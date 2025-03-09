1a55c11991cdad9598308a47e24ab885cae5da15
Fernandes Júnior <fernaandes.jr@gmail.com>
2025-03-09 15:08:50 -0300

    end jwt branch

--- obj/Debug/net8.0/TaskApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04446636b643bfe05f956ff307e6be19ccbd10b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21efdc5978ccc85ddb9c8d6753457eef499be9f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
